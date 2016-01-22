dfc11a11ba955559f4e8e5c81f015b484df901e1
eMdOS <emilio.ojeda.mendoza@gmail.com>
2016-01-22 17:35:08 -0600

    Updated AirPlay pod to v1.3.0 and sample code

--- Example/AirPlay/AppDelegate.swift
+++ Example/AirPlay/AppDelegate.swift
@@ -42,6 +42,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
 }
 
